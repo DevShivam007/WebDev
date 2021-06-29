6ff68ab479f02a9b54de74914e03cde9fe305c75
DevShivam007 <handa.edtech02@gmail.com>
2021-06-29 13:17:43 +1000

    increasingPatternLogic

--- 20210616_basiclayout/includes/action/print.php
+++ 20210616_basiclayout/includes/action/print.php
@@ -1,7 +1,48 @@
 <?php
-
-echo "<pre>";
-print_r($_REQUEST);
-echo "<pre>";
-echo $_REQUEST['fname'];
-?>
+if(isset($_POST['patternId']) && !empty($_POST['patternId']))
+{
+    if(isset($_POST['symbolId']) && !empty($_POST['symbolId']))
+    {
+        $i=$_POST['patternId'];
+        $s=$_POST['symbolId']; 
+           
+        for($k=1;$k<=$i;$k++) 
+        {
+            for($j=1;$j<$k;$j++)
+            {
+                echo "$s";
+            }
+            echo "<br>";
+            if($j==$i)
+            {
+                $con=$j;
+                for($m=0;$m<=$con;$m++)
+               {
+                for($n=$con-$m;$n>=1;$n--)
+                   {
+                    echo "$s";
+                    }
+                echo "<br>";
+                } 
+            }
+        }
+    }
+    else
+    {
+        $i=$_POST['patternId'];
+        $j=1;
+        for($k=1;$k<=$i;$k++) 
+        {     
+            for($j=1;$j<=$k;$j++)
+            {
+                echo "$j";
+            }
+            echo "<br>";
+        }
+    }
+}
+ else
+    {
+    echo "Go Back and Fill form";
+    }
+    ?>
